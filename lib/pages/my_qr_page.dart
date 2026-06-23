51d87b408f2026734688a904d75b4f42250af055
unknown <gdonaldo1997@gmail.com>
2026-06-22 23:17:35 -0500

    mi repositorio setting y qr

---
+++ lib/pages/my_qr_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyQrPage extends StatelessWidget {
+  const MyQrPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("QR Scanner"),
+      ),
+      body: const Center(
+        child: Icon(
+          Icons.qr_code_scanner,
+          size: 150,
+        ),
+      ),
+    );
+  }
+}
