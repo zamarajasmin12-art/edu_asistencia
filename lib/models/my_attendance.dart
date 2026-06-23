f58e78991d21c234115deb26c5b20dae53de52ec
Jose Carlos Manchego <72034898@certus.edu.pe>
2026-06-22 21:12:40 -0500

    Subiendo Models
    Subiendo Models

---
+++ lib/models/my_attendance.dart
@@ -0,0 +1,13 @@
+class MyAttendance {
+  int? id;
+  int studentId;
+  String date;
+  String status;
+
+  MyAttendance({
+    this.id,
+    required this.studentId,
+    required this.date,
+    required this.status,
+  });
+}
---
+++ lib/models/my_student.dart
@@ -0,0 +1,13 @@
+class MyStudent {
+  int? id;
+  String name;
+  String code;
+  String workshop;
+
+  MyStudent({
+    this.id,
+    required this.name,
+    required this.code,
+    required this.workshop,
+  });
+}
