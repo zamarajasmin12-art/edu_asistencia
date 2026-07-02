f1f4f542419b107ed95e7c2414d85e18e47d8bff
70851660-bit <70851660@certus.edu.pe>
2026-07-01 22:13:52 -0500

    Delete src directory

--- src/routes/asistenciaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const asistenciaController = require('../controllers/asistenciaController');
-
-router.get('/curso-actual', asistenciaController.desplegarCurso);
-router.post('/registrar', asistenciaController.marcarAsistencia);
-
-module.exports = router;
